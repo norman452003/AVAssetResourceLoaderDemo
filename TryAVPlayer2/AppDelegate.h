8439e2a122d3caea5b341fd63d0194fd65dc8a20
gongxin <gongxin@autohome.com.cn>
2017-01-10 10:44:57 +0800

    Initial Commit

---
+++ TryAVPlayer2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryAVPlayer2
+//
+//  Created by gongxin on 17/1/10.
+//  Copyright © 2017年 gongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryAVPlayer2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryAVPlayer2
+//
+//  Created by gongxin on 17/1/10.
+//  Copyright © 2017年 gongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
